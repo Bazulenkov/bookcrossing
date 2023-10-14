9a7fedce75a9b25372eecac6127f88acae26284e
Вася Васин <horde_rhythms07@icloud.com>
2023-10-14 22:34:56 +0300

    adjust CATEGORIES according to BOOKS

--- bookcrossing_project/catalog/urls.py
+++ bookcrossing_project/catalog/urls.py
@@ -6,7 +6,7 @@ app_name = 'catalog'
 
 urlpatterns = [
     path('categories/', views.categories_view, name='categories'),
-    path('categories/<int:category_id>', views.category_detail_view, name='category_detail'),
+    path('categories/<str:category_slug>', views.category_detail_view, name='category_detail'),
     path('book/<int:book_id>', views.book_detail_view, name='book_detail'),
     path('', views.index_view, name='index'),
 ]
--- bookcrossing_project/catalog/views.py
+++ bookcrossing_project/catalog/views.py
@@ -1,6 +1,7 @@
+from django.http import Http404, HttpResponse
 from django.shortcuts import render
 
-from data import BOOKS
+from data import BOOKS, CATEGORIES
 
 def index_view(request):
     context = {'books':BOOKS}
@@ -8,15 +9,33 @@ def index_view(request):
 
 
 def categories_view(request):
-    return render(request, 'catalog/categories.html')
-
-
-def category_detail_view(request, category_id):
-    return render(request, 'catalog/category.html')
+    context = {'categories':CATEGORIES}
+    return render(request, 'catalog/categories.html', context)
 
 
 books_ = {book['id']:book for book in BOOKS}
+category_book_list = {cat: [] for cat in set(category 
+                        for book in books_.values() 
+                        for category in book['category'])}
+
+
+for book in books_.values():
+    for cat in book['category']:
+        category_book_list[cat].append(book)
+
+
+def category_detail_view(request, category_slug):
+        if category_slug in category_book_list:
+            context = {'category': category_slug,
+                    'category_book_list': category_book_list[category_slug]}
+            return render(request, 'catalog/category.html', context)
+        else:
+            raise Http404(f'Страница с category: {category_slug} не существует...')
+
 
 def book_detail_view(request, book_id):
-    context ={'book': books_[book_id]}
-    return render(request, 'catalog/book.html', context)
+    if book_id in books_:
+        context ={'book': books_[book_id]}
+        return render(request, 'catalog/book.html', context)
+    else:
+        raise Http404(f'Страница с book_id: {book_id} не существует...')
