35314bb7403d27932e88626a34faf86548508565
Bazulenkov <anton@bazul.ru>
2023-10-14 14:26:45 +0400

    feat: big part of content.

--- bookcrossing_project/catalog/urls.py
+++ bookcrossing_project/catalog/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'catalog'
 
 urlpatterns = [
-    path('categories', views.categories_view, name='categories'),
+    path('categories/', views.categories_view, name='categories'),
     path('categories/<int:id>/', views.category_detail_view, name='category_detail'),
     path('book/<int:id>/', views.book_detail_view, name='book_detail'),
 
--- bookcrossing_project/catalog/views.py
+++ bookcrossing_project/catalog/views.py
@@ -1,12 +1,21 @@
+from data import BOOKS, CATEGORIES
+
+from django.http import Http404
 from django.shortcuts import render
 
 
 def index_view(request):
-    return render(request, 'catalog/index.html')
+    # BOOKS = Получение объектов из БД
+    context = {'books': BOOKS}
+    return render(request, 'catalog/index.html', context)
 
 
 def categories_view(request):
-    return render(request, 'catalog/categories.html')
+    return render(
+        request,
+        'catalog/categories.html',
+        {"categoties": CATEGORIES}
+    )
 
 
 def category_detail_view(request, id):
@@ -14,4 +23,8 @@ def category_detail_view(request, id):
 
 
 def book_detail_view(request, id):
-    return render(request, 'catalog/book.html')
+    for book in BOOKS:
+        if book['id'] == id:
+            context = {'book': book}
+            return render(request, 'catalog/book.html', context)
+    raise Http404
